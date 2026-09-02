f761e999cc21e4b817d4368116405327849ce77a
hieuliem <36899535+hieuliem@users.noreply.github.com>
2026-09-02 13:30:33 -0400

    Add recurring event key migration

---
+++ drizzle/0002_event_keys.sql
@@ -0,0 +1,9 @@
+DO $$
+BEGIN
+  ALTER TABLE celebrations ADD COLUMN IF NOT EXISTS event_key text;
+  UPDATE celebrations
+  SET event_key = regexp_replace(slug, '-[0-9]{4}-[0-9]{2}-[0-9]{2}$', '')
+  WHERE event_key IS NULL;
+  CREATE INDEX IF NOT EXISTS idx_celebrations_event_key ON celebrations(event_key);
+END $$;
+
